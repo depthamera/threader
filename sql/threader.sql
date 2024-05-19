8bc7a01799675308fa2ed2352f4325b27f8aae53
depthamera <shet14886@gmail.com>
2024-05-20 07:34:20 +0900

    db sql added.

---
+++ sql/threader.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- 생성 시간: 24-05-20 00:32
+-- 서버 버전: 10.4.32-MariaDB
+-- PHP 버전: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `threader`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `member`
+--
+
+CREATE TABLE `member` (
+  `Member_ID` varchar(20) NOT NULL,
+  `Member_Password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 테이블의 덤프 데이터 `member`
+--
+
+INSERT INTO `member` (`Member_ID`, `Member_Password`) VALUES
+('test', '1234');
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`Member_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+GRANT USAGE ON *.* TO `threader_user`@`localhost` IDENTIFIED BY PASSWORD '*97E7471D816A37E38510728AEA47440F9C6E2585';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON `threader`.* TO `threader_user`@`localhost`;
