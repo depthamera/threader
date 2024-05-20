d29d94d6ee35712e8f1ef69a276c8e33e0f18b21
depthamera <shet14886@gmail.com>
2024-05-20 09:28:35 +0900

    sign up system completed.

--- script/sign_up/id_duplication_check.php
+++ script/sign_up/id_duplication_check.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
     $user_id = $_GET["user_id"];
 
-    $con = mysqli_connect("localhost","threader_user","0000","threader");
+    //아이디 중복 검사
+    $con = mysqli_connect("localhost", "threader_user", "0000", "threader");
     $sql = "SELECT * FROM `member` WHERE `member_id` = '$user_id'";
-    $result = mysqli_query($con,$sql);
+    $result = mysqli_query($con, $sql);
     $row = mysqli_fetch_array($result);
 
-    if($row) {
+    //검사 결과 출력 
+    if ($row) {
         echo "이미 존재하는 아이디입니다.";
-    }
-    else {
+    } else {
         echo "사용 가능한 아이디입니다.";
     }
     ?>
     <br>
-    
+
     <button onclick="window.close()">확인</button>
 </body>
+
 </html>
