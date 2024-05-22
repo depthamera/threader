459f5555a22978aceec8d68e3c14486a66e0625c
depthamera <shet14886@gmail.com>
2024-05-22 10:52:02 +0900

    add new thread system completed.

--- script/threads/thread_submit.php
+++ script/threads/thread_submit.php
@@ -2,16 +2,25 @@
 $title = $_POST["title"];
 $comment = $_POST["comment"];
 
-//새 스레드 생성
+//새 스레드 등록
 $con = mysqli_connect("localhost", "threader_user", "0000", "threader");
-$sql = "INSERT INTO `thread` VALUES ()";
+$sql = "INSERT INTO thread(title, comment) VALUES('$title', '$comment')";
 mysqli_query($con, $sql);
 
 //생성한 스레드의 번호 가져오기
-$sql = "SELECT `thread_id` FROM `thread` ORDER BY 1 DESC LIMIT 1";
+$sql = "SELECT thread_id FROM thread ORDER BY 1 DESC LIMIT 1";
 $thread_id = mysqli_fetch_array(mysqli_query($con, $sql))[0];
 
 //가져온 스레드 번호로 코멘트 등록
-$sql = "INSERT INTO `comment`(`thread_id`, `title`, `comment`) VALUES ($thread_id, '$title', $comment')";
+$sql = "INSERT INTO comment(thread_id, title, comment) VALUES ($thread_id, '$title', '$comment')";
 mysqli_query($con, $sql);
+
+//등록한 코멘트의 번호 가져오기
+$sql = "SELECT comment_id FROM comment ORDER BY 1 DESC LIMIT 1";
+$comment_id = mysqli_fetch_array(mysqli_query($con, $sql))[0];
+
+//생성했던 스레드의 최근 코멘트 아이디를 변경
+$sql = "UPDATE thread SET comment_id_last = $comment_id WHERE thread_id = $thread_id";
+
+//생성한 스레드로 이동
 echo "<script>location.replace('thread.php?thread_id=$thread_id')</script>";
--- script/threads/threads.php
+++ script/threads/threads.php
@@ -15,17 +15,29 @@
     function draw_thread($row)
     {
         $thread_id = $row["thread_id"];
-        echo ("<a href=thread.php?thread_id=$thread_id>$thread_id</a><br>");
+        $title = $row["title"];
+        $comment = $row["comment"];
+
+
+        echo ("<a href=thread.php?thread_id=$thread_id>
+                $thread_id | $title - $comment
+            </a><br>");
     }
 
     //최근에 업데이트된 스레드부터 내림차순으로 정렬
     $con = mysqli_connect("localhost", "threader_user", "0000", "threader");
-    $sql = "SELECT rk.`thread_id` 
-            FROM ( 
-                SELECT *, ROW_NUMBER() OVER (PARTITION BY `thread_id` ORDER BY `comment_id` DESC) AS rn 
-                FROM `comment`) AS rk
-            WHERE rn = 1
-            ORDER BY rk.`comment_id` DESC;";
+    $sql = "SELECT c1.thread_id, c1.title, c1.comment
+            FROM (
+                SELECT *, 
+                    ROW_NUMBER() OVER(PARTITION BY thread_id ORDER BY comment_id) idx
+                FROM comment) c1
+            JOIN (
+                SELECT DISTINCT thread_id, 
+                    MAX(comment_id) OVER(PARTITION BY thread_id) last_id
+                FROM comment) c2
+            ON c1.thread_id = c2.thread_id
+            WHERE c1.idx = 1
+            ORDER BY c2.last_id DESC;";
 
     $result = mysqli_query($con, $sql);
 
@@ -49,7 +61,7 @@
     <form action="thread_submit.php" method="post" name="thread_post">
         <input type="text" name="title" id="title" placeholder="제목"><br>
         <textarea name="comment" id="comment" cols="30" rows="10" placeholder="내용"></textarea>
-        <button type="button">게시</button>
+        <button type="sumbit">게시</button>
     </form>
 </body>
 
