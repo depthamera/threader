67db1849e688267f67fa33e7c1d180e36fdc9344
depthamera <shet14886@gmail.com>
2024-05-28 12:02:29 +0900

    ㅁㄴㅇㄹ

--- script/header/header.php
+++ script/header/header.php
@@ -1,32 +1,34 @@
-<head>
+<style>
+    <?php include "header.css"; ?>
+</style>
+<?php
+session_start();
+$is_admin = (isset($_SESSION["permission"]) && $_SESSION["permission"] == "admin");
+?>
+<header>
     <h3>이것은 헤더입니다</h3>
-    <?php
-    session_start();
-    $is_admin = (isset($_SESSION["permission"]) && $_SESSION["permission"] == "admin");
-    ?>
-    <header>
-        <script>
-            const handleKeyPress = (e) => {
-                if (e.keyCode == 13) {
-                    const keyword = document.getElementById('keyword').value;
-                    if (keyword) {
-                        location.href = `../threads/search.php?page=1&keyword=${keyword}`;
-                    }
+    <script>
+        const handleKeyPress = (e) => {
+            if (e.keyCode == 13) {
+                const keyword = document.getElementById('keyword').value;
+                if (keyword) {
+                    location.href = `../threads/search.php?page=1&keyword=${keyword}`;
                 }
             }
-        </script>
-        <?php $con = mysqli_connect("localhost", "threader_user", "0000", "threader"); ?>
-        <a href="../main/main.php">THREADER</a>
-        <a href="../threads/threads.php?page=1">스레드 목록</a>
-        <input type="text" name="keyword" id="keyword" onkeypress="handleKeyPress(event)" placeholder="검색">
-        <?php
-        if (isset($_SESSION["member_id"]))
-            echo "<a href='../account/logout.php'>로그아웃</a>";
-        else {
-            echo "<a href='../account/sign_in.php'>로그인</a> ";
-            echo "<a href='../account/sign_up.php'>회원가입</a>";
         }
-        ?>
-        <hr>
-    </header>
-</head>
+    </script>
+    <?php $con = mysqli_connect("localhost", "threader_user", "0000", "threader"); ?>
+    <a href="../main/main.php">메인</a>
+    <a href="../threads/threads.php?page=1">스레드 목록</a>
+    <input type="text" name="keyword" id="keyword" onkeypress="handleKeyPress(event)" placeholder="검색">
+    <?php
+    if (isset($_SESSION["member_id"])) {
+        echo "<a href='../account/member_info.php'>회원정보</a> ";
+        echo "<a href='../account/logout.php'>로그아웃</a>";
+    } else {
+        echo "<a href='../account/sign_in.php'>로그인</a> ";
+        echo "<a href='../account/sign_up.php'>회원가입</a>";
+    }
+    ?>
+    <hr>
+</header>
--- script/main/main.php
+++ script/main/main.php
@@ -12,7 +12,7 @@
   <h1>메인 페이지입니다</h1>
   <?php
   if (!isset($_SESSION["member_id"])) { ?>
-    <h3>게시물에 작성자 아이디를 남기고 싶다면</h3>
+    <h3>회원으로 이용하기</h3>
     <a href="../account/sign_in.php">로그인</a>
     <br>또는<br>
     <a href="../account/sign_up.php">회원가입</a>
@@ -21,7 +21,7 @@
   }
   ?>
 
-  <h3>지금 인기 있는 스레드는 뭘까요?</h3>
+  <h3>지금 인기 있는 스레드들</h3>
   <a href="../threads/threads.php?page=1">스레드 구경하기</a>
 </body>
 
